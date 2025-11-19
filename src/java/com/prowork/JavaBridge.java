2ea9a95610a7d7edc6f34e543b8bcb9585c4d9c1
Pesho08 <christianmitev1@gmail.com>
2025-11-19 11:08:24 +0100

    initial commit of ProWork project

---
+++ src/java/com/prowork/JavaBridge.java
@@ -0,0 +1,9 @@
+package com.prowork;
+
+import javafx.scene.web.WebEngine;
+
+public class JavaBridge {
+  private final WebEngine engine;
+
+  public JavaBridge(WebEngine engine) { this.engine = engine; }
+}
